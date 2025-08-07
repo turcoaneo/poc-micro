e6a76cc130fefa6590d30fe7b444e3d95322fe61
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2025-08-07 09:04:27 +0300

    Refactoring. Small logging.

--- employer-app/src/main/java/com/poc/microservices/employer/app/config/JwtAuthFilterEM.java
+++ employer-app/src/main/java/com/poc/microservices/employer/app/config/JwtAuthFilterEM.java
@@ -26,6 +26,8 @@ public class JwtAuthFilterEM extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         String requestUri = request.getRequestURI();
 
+        logger.info("Request URI: {}", requestUri);
+
         if (requestUri == null) return;
 
         List<String> excludedEndpoints = Arrays.asList("/em/em-users/login", "/em/em-users/register",
